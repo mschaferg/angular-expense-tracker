51211424582be9b566a38f1f3ccad13033261421
Schafer Gray <schafergray@Schafers-MacBook-Air.local>
2023-11-19 13:21:00 -0500

    Updating api url in other locations.

--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
    providedIn: 'root'
 })
 export class ExpenseService {
-   private apiUrl = 'http://localhost:3000';
+   private apiUrl = 'http://node-expense-tracker-production.up.railway.app';
 
    constructor(private http: HttpClient) {}
 
